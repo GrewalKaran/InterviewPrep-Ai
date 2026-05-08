62ab10842f9f0589511eb08d905a3762df486d58
Karan grewal <136038074+GrewalKaran@users.noreply.github.com>
2026-05-09 01:41:02 +0530

    Update CORS origin for vercel frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors({
   origin: [
-    "https://interview-prep-ai-olive.vercel.app/login",
+    "https://interviewprep-ai-4hh0.onrender.com",
     "http://localhost:5173"
   ],
   credentials: true
